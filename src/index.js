8fdf9152d2cc11dfeb24324fc026118c544982b0
Xavier Joliot <71592312+XavierJoliot@users.noreply.github.com>
2022-06-20 14:23:02 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // == Import : npm
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,13 +12,15 @@ import store from 'src/store';
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>
+  <BrowserRouter>
+      <Provider store={store}>
+          <App />
+      </Provider>
+  </BrowserRouter>
 );
+
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
-const target = document.getElementById('root');
+const root = createRoot(document.getElementById('root'));
+
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
-render(rootReactElement, target);
+root.render(rootReactElement);
