43f9bc529889311e0d92d006157ad72ab68a79e6
Xavier Joliot <71592312+XavierJoliot@users.noreply.github.com>
2022-07-06 15:43:22 -0400

    responsive home page ok

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,9 +8,9 @@ import InternPageHeadSection from '../../shared/internPageHeadSection';
 import ContactHead from '../../assets/images/contact-head.jpg';
 
 const Contact = () => (
-  <main className='contact'>
-    <InternPageHeadSection title='Contact' imagePath={ContactHead} />
-    <section className='contact__description'>
+  <main className='contact-page'>
+    <InternPageHeadSection title='Contact-page' imagePath={ContactHead} />
+    <section className='contact-page__description'>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent enim mauris, dictum eget sapien vitae, auctor vulputate mi. Maecenas finibus elit arcu, vel sodales massa vulputate quis. Praesent iaculis risus vitae feugiat laoreet. Vestibulum ut ligula nec mi rhoncus sodales.</p>
     </section>
   </main>
