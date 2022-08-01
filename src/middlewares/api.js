3e0f287e5c302903fbcebe3c07615eaebf3e41af
Xavier Joliot <71592312+XavierJoliot@users.noreply.github.com>
2022-07-31 22:27:48 -0400

    fix: optimise API call

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ALL_RECIPES, setAllRecipes, SUBMIT_ADD_RECIPE_MODAL } from '../actions/recipes';
+import { getAllRecipes, GET_ALL_RECIPES, setAllRecipes, setIsModalOpen, SUBMIT_ADD_RECIPE_MODAL } from '../actions/recipes';
 
 const apiMiddleware = (store) => (next) => (action) => {
   axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
@@ -7,7 +7,10 @@ const apiMiddleware = (store) => (next) => (action) => {
   const { token } = store.getState().user;
 
   const api = axios.create({
-    baseURL: 'https://localhost:7262/'
+    baseURL: 'https://localhost:7262/',
+    headers: {
+      'authorization': `Bearer ${token}`
+    },
   });
 
   switch (action.type) {
@@ -15,16 +18,11 @@ const apiMiddleware = (store) => (next) => (action) => {
       const { newRecipe } = store.getState().addRecipeModal;
       api
         .post('/api/recette',
-          {
-            crossdomain: true,
-            header: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
-          newRecipe,)
+          newRecipe)
         .then(
           () => {
-            console.log('ok')
+            store.dispatch(getAllRecipes());
+            store.dispatch(setIsModalOpen());
           },
         )
         .catch((error) => {
@@ -38,13 +36,7 @@ const apiMiddleware = (store) => (next) => (action) => {
     case GET_ALL_RECIPES: {
       const { token } = store.getState().user;
       api
-        .get('api/recette/',
-        {
-          headers: {
-            'authorization': `Bearer ${token}`
-
-          },
-        })
+        .get('api/recette/')
         .then(
           (response) => {
             store.dispatch(setAllRecipes(response.data));
