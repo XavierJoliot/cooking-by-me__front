c7699e2c3b3ff2e2d1925c0a7a9eceaaae1fad50
Xavier Joliot <71592312+XavierJoliot@users.noreply.github.com>
2022-08-04 13:20:16 -0400

    fix: user-stories updated

--- docs/userStories.md
+++ docs/userStories.md
@@ -1,23 +1,27 @@
 # USER STORIES FRONT
 
 ## Visiteur :
-| En tant que | je veux | afin de |
-| -- | -- | -- |
-| Visiteur | comprendre le principe du site | |
-| Visiteur | consulter des recettes existantes | |
-| Visiteur | me créer un compte | afin de pouvoir créer mon livre de recette |
-| Visiteur | me connecter à mon compte | |
+| En tant que | je veux | afin de | MVP version |
+| -- | -- | -- | -- |
+| Visiteur | comprendre le principe du site | | v1 |
+| Visiteur | consulter des recettes existantes | | v1 |
+| Visiteur | me créer un compte | pouvoir créer mon livre de recette | v1 |
+| Visiteur | me connecter à mon compte | | v1 |
 
 ## Utilisateur :
-| En tant que | je veux | afin de |
-| -- | -- | -- |
-| Utilisateur | créer mes recettes | |
-| Utilisateur | consulter mes recette | |
-| Utilisateur | modifier mes recettes | |
-| Utilisateur | supprimer mes recettes | |
-| Utilisateur | recenser mes recettes par thématique | afin de pouvoir trier mon contenu |
-| Utilisateur | ajouter une image à mes recettes | |
-| Utilisateur | accéder à mon profil | |
-| Utilisateur | me déconnecter | |
-| Utilisateur | contacter un support | |
-| Utilisateur | trier mes recettes | afin de limiter mon temps de recherche |
+| En tant que | je veux | afin de | MVP version |
+| -- | -- | -- | -- |
+| Utilisateur | créer mes recettes | | v1 |
+| Utilisateur | consulter mes recette | | v1 |
+| Utilisateur | modifier mes recettes | | v1 |
+| Utilisateur | supprimer mes recettes | | v1 |
+| Utilisateur | ajouter une image à mes recettes | | v1 |
+| Utilisateur | me déconnecter | | v1 |
+| Utilisateur | contacter un support | | v1 |
+| Utilisateur | recenser mes recettes par thématique | pouvoir trier mon contenu | v2 |
+| Utilisateur | trier mes recettes | limiter mon temps de recherche | v2 |
+| Utilisateur | ajouter des recettes dans une list de favoris | | v2 |
+| Utilisateur | accéder à mon profil | | v2 |
+| Utilisateur | partager mes recettes | | v3 |
+| Utilisateur | changer le nombre de personne de la recette | adapter la quantité des ingrédients | v3 |
+| Utilisateur | autoriser des membres à consulter mes recette | | v3 |
--- src/shared/footerMenu/index.js
+++ src/shared/footerMenu/index.js
@@ -1,34 +1,34 @@
 // import utils
 import './styles.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 // import images
 import Logo from '../../assets/images/logo_typo.png';
 
 const FooterMenu = () => (
   <nav className='footer-navigation'>
-    <Link className='footer-navigation__link' to='/' target='_blank'>
+    <NavLink className='footer-navigation__link' to='/' target='_blank'>
       <img className='footer-navigation__link__logo' src={Logo} alt='Logo typographie Cooking by me' />
-    </Link>
+    </NavLink>
     <ul className='footer-navigation__list'>
-      <Link className='footer-navigation__list__link' to='/' target='_blank'>
+      <NavLink className='footer-navigation__list__link' to='/' target='_blank'>
         <li className='footer-navigation__list__link__item'>Cooking by me</li>
-      </Link>
-      <Link className='footer-navigation__list__link' to='a-propos-de-nous' target='_blank'>
+      </NavLink>
+      <NavLink className='footer-navigation__list__link' to='a-propos-de-nous' target='_blank'>
         <li className='footer-navigation__list__link__item'>À propos de nous</li>
-      </Link>
-      <Link className='footer-navigation__list__link' to='mes-recettes' target='_blank'>
+      </NavLink>
+      <NavLink className='footer-navigation__list__link' to='mes-recettes' target='_blank'>
         <li className='footer-navigation__list__link__item'>Mes recettes</li>
-      </Link>
-      <Link className='footer-navigation__list__link' to='#' target='_blank'>
+      </NavLink>
+      <NavLink className='footer-navigation__list__link' to='#' target='_blank'>
         <li className='footer-navigation__list__link__item'>Mon espace</li>
-      </Link>
-      <Link className='footer-navigation__list__link' to='contact' target='_blank'>
+      </NavLink>
+      <NavLink className='footer-navigation__list__link' to='contact' target='_blank'>
         <li className='footer-navigation__list__link__item'>Contact</li>
-      </Link>
-      <Link className='footer-navigation__list__link' to='mentions-legales' target='_blank'>
+      </NavLink>
+      <NavLink className='footer-navigation__list__link' to='mentions-legales' target='_blank'>
         <li className='footer-navigation__list__link__item'>Mentions légales</li>
-      </Link>
+      </NavLink>
     </ul>
   </nav>
 );
