49dee0c7b095a993331df5d2e212b1ceb988ca87
Xavier Joliot <71592312+XavierJoliot@users.noreply.github.com>
2022-08-04 13:11:21 -0400

    fix: fix footermenu lin

--- src/shared/footerMenu/index.js
+++ src/shared/footerMenu/index.js
@@ -1,33 +1,34 @@
 // import utils
 import './styles.scss';
+import { Link } from 'react-router-dom';
 
 // import images
 import Logo from '../../assets/images/logo_typo.png';
 
 const FooterMenu = () => (
   <nav className='footer-navigation'>
-    <a className='footer-navigation__link' href='/' target='_blank'>
+    <Link className='footer-navigation__link' to='/' target='_blank'>
       <img className='footer-navigation__link__logo' src={Logo} alt='Logo typographie Cooking by me' />
-    </a>
+    </Link>
     <ul className='footer-navigation__list'>
-      <a className='footer-navigation__list__link' href='/' target='_blank'>
+      <Link className='footer-navigation__list__link' to='/' target='_blank'>
         <li className='footer-navigation__list__link__item'>Cooking by me</li>
-      </a>
-      <a className='footer-navigation__list__link' href='/a-propos-de-nous' target='_blank'>
+      </Link>
+      <Link className='footer-navigation__list__link' to='a-propos-de-nous' target='_blank'>
         <li className='footer-navigation__list__link__item'>À propos de nous</li>
-      </a>
-      <a className='footer-navigation__list__link' href='mes-recettes' target='_blank'>
+      </Link>
+      <Link className='footer-navigation__list__link' to='mes-recettes' target='_blank'>
         <li className='footer-navigation__list__link__item'>Mes recettes</li>
-      </a>
-      <a className='footer-navigation__list__link' href='#' target='_blank'>
+      </Link>
+      <Link className='footer-navigation__list__link' to='#' target='_blank'>
         <li className='footer-navigation__list__link__item'>Mon espace</li>
-      </a>
-      <a className='footer-navigation__list__link' href='/contact' target='_blank'>
+      </Link>
+      <Link className='footer-navigation__list__link' to='contact' target='_blank'>
         <li className='footer-navigation__list__link__item'>Contact</li>
-      </a>
-      <a className='footer-navigation__list__link' href='/mentions-legales' target='_blank'>
+      </Link>
+      <Link className='footer-navigation__list__link' to='mentions-legales' target='_blank'>
         <li className='footer-navigation__list__link__item'>Mentions légales</li>
-      </a>
+      </Link>
     </ul>
   </nav>
 );
